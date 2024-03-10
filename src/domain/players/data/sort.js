bd30bd9056dd17031a0bf39d604350003e3d1114
Sam Peterson <samuel.e.peterson@gmail.com>
2024-03-09 22:03:19 -0600

    fix: correct sorting to always show empty values at bottom

--- src/domain/players/data/sort.js
+++ src/domain/players/data/sort.js
@@ -1,8 +1,14 @@
 /* Sort Players */
 function sortPlayers(players, columnKey, direction) {
+    console.log(columnKey)
+
     const sortedPlayers = players.sort((a, b) => {
         const aVal = a[columnKey]
         const bVal = b[columnKey]
+
+        if (!aVal && bVal) return 1
+        if (aVal && !bVal) return -1
+        if (!aVal && !bVal) return 0
 
         if (direction === "asc") {
             if (aVal < bVal) return -1
